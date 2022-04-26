07e36591040cfab6a8c3ad00db9098ea288c8c55
Tim Jackson <timjacksonm@gmail.com>
2022-04-26 17:26:52 -0500

    you can now query by department name or id and also get all employees within that department

--- index.js
+++ index.js
@@ -14,7 +14,37 @@ const app = express();
 
 const PeopleType = new GraphQLObjectType({
   name: 'People',
-  description: 'Employees of the organization',
+  description: 'This is a person apart of the organization',
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLString) },
+    firstName: { type: GraphQLNonNull(GraphQLString) },
+    lastName: { type: GraphQLNonNull(GraphQLString) },
+    jobTitle: { type: GraphQLNonNull(GraphQLString) },
+    departmentId: { type: GraphQLNonNull(GraphQLString) },
+    managerId: { type: GraphQLString },
+  }),
+});
+
+const DepartmentType = new GraphQLObjectType({
+  name: 'Department',
+  description: 'This is a department of the organization.',
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLString) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    employees: {
+      type: new GraphQLList(EmployeeType),
+      resolve: (department) => {
+        return UserData.people.filter(
+          (employee) => employee.departmentId === department.id
+        );
+      },
+    },
+  }),
+});
+
+const EmployeeType = new GraphQLObjectType({
+  name: 'Employee',
+  description: 'This represents a list of employees',
   fields: () => ({
     id: { type: GraphQLNonNull(GraphQLString) },
     firstName: { type: GraphQLNonNull(GraphQLString) },
@@ -31,7 +61,33 @@ const RootQueryType = new GraphQLObjectType({
   fields: () => ({
     ceo: {
       type: PeopleType,
+      description: 'This is the CEO of the organization.',
       resolve: () => UserData.people.find((user) => user.jobTitle === 'CEO'),
+    },
+    departments: {
+      type: new GraphQLList(DepartmentType),
+      description: 'List of all departments within the organization.',
+      args: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+      },
+      resolve: (parent, args) => {
+        if (args?.id) {
+          return [
+            UserData.departments.find(
+              (department) => department.id === args.id
+            ),
+          ];
+        }
+        if (args?.name) {
+          return [
+            UserData.departments.find(
+              (department) => department.name === args.name
+            ),
+          ];
+        }
+        return UserData.departments;
+      },
     },
   }),
 });
